ba081a2782e1c2cb10af58ec3307a1290ea306b2
LuZhenqian <luzhenqian98@gmail.com>
2022-10-21 14:58:55 +0800

    feat: prisma

---
+++ prisma/db.ts
@@ -0,0 +1,63 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+prisma.$use(async (params, next) => {
+  if (params.action === "findUnique" || params.action === "findFirst") {
+    params.action = "findFirst";
+    params.args.where["deletedAt"] = null;
+  }
+  if (params.action === "findMany") {
+    if (params.args.where) {
+      if (params.args.where.deleted === undefined) {
+        params.args.where["deletedAt"] = null;
+      }
+    } else {
+      params.args["where"] = { deletedAt: null };
+    }
+  }
+  return next(params);
+});
+
+prisma.$use(async (params, next) => {
+  if (params.model == "Post") {
+    if (params.action == "update") {
+      params.action = "updateMany";
+      params.args.where["deletedAt"] = null;
+    }
+    if (params.action == "updateMany") {
+      if (params.args.where !== undefined) {
+        params.args.where["deletedAt"] = null;
+      } else {
+        params.args["where"] = { deletedAt: null };
+      }
+    }
+  }
+  return next(params);
+});
+
+prisma.$use(async (params, next) => {
+  if (params.action == "delete") {
+    params.action = "update";
+    params.args["data"] = { deletedAt: new Date() };
+  }
+  if (params.action == "deleteMany") {
+    params.action = "updateMany";
+    if (params.args.data != undefined) {
+      params.args.data["deletedAt"] = new Date();
+    } else {
+      params.args["data"] = { deletedAt: new Date() };
+    }
+  }
+  return next(params);
+});
